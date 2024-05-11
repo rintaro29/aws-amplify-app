b866429b828a006147992a48ac567d7004b65a49
rintaro1029 <tomitaka1029@gmail.com>
2024-05-12 01:16:33 +0900

    setting auth function

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "awstutorialwithnext7dd25f22": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,35 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { Amplify } from "aws-amplify";
 
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import config from "../src/amplifyconfiguration.json";
+Amplify.configure(config);
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-  return <div>Hello AWS</div>;
+interface AuthProps {
+  signOut: () => void;
+  user: { username: string };
 }
+
+const Home = ({ signOut, user }: AuthProps) => {
+  return (
+    <div>
+      {user ? (
+        <>
+          <h3>私は権限を持っています:{user.username}</h3>
+          <button className="bg-cyan-200 rounded-lg text-blue-600 hover:text-red-400" onClick={signOut}>
+            サインアウト
+          </button>
+        </>
+      ) : (
+        <>
+          <h3>私は権限を持っていません</h3>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default withAuthenticator(Home);
